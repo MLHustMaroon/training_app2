4fcb270613444059a4ee67bcf2966120fae39cf3
MLHustMaroon <doxuan.thanh@monstar-lab.com>
2017-11-30 13:19:58 +0900

    fix test

--- app/models/tag.rb
+++ app/models/tag.rb
@@ -1,6 +1,6 @@
 class Tag < ApplicationRecord
   has_and_belongs_to_many :tasks
   validates :label, uniqueness: true, presence: true
-  before_save { self.label = label.gsub(/\s+/, '') }
+  before_save { self.label = self.label.gsub(/\s+/, '') }
 
 end
